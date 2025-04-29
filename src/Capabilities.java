f5180919eb4d9fe88c147b13b0da388bd2cf1535
Amr Ashraf <amr200000004@gmail.com>
2025-04-29 21:22:23 +0300

    Added The Capabilites Interface & added Empty Main and Item classes

---
+++ src/Capabilities.java
@@ -0,0 +1,10 @@
+public interface Capabilities
+{
+    void add_item(Item i);
+    void remove_item(Item i);
+    void remove_item_by_id(String id);
+    void sell_item(Item i);
+    void sell_item_by_id(String id);
+    void find_item_by_name (String name);
+    void find_item_by_id  (String id) ;
+}
